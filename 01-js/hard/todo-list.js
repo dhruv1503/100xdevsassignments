d4e3fcbe908816b46e0c182c7b42ef92242ab1ba
Dhruv Aggarwal <dhruvaggarwal1503@gmail.com>
2024-05-29 02:43:59 +0530

    sol: calculator, todo and fix: countVowel

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,32 @@
 */
 
 class Todo {
-
+constructor(){
+  this.list = [];
 }
+add(task) {
+  this.list.push(task)
+}
+remove(index){
+  const removedTaskArr = this.list.filter((_, i) => (i !== index));
+  this.list = removedTaskArr
+}
+update(index, task){
+  const updatedTaskArr = this.list.map((element, i) => (index === i ? task : element));
+  this.list = updatedTaskArr
+}
+getAll(){
+  return this.list
+}
+get(index){
+  const requiredTask = this.list.find((_, i) => (index === i));
+  return requiredTask ? requiredTask : null
+}
+clear(){
+  this.list = []
+}
+}
+
+const todoList = new Todo()
 
 module.exports = Todo;
