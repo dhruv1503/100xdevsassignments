75789023e21b0694e9bbb393dfe38333c41b65e6
Dhruv Aggarwal <dhruvaggarwal1503@gmail.com>
2024-05-29 00:12:37 +0530

    sol: isAnagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,46 @@
 */
 
 function isAnagram(str1, str2) {
+  if (!str1 && !str2) return false;
 
+  if (!str1 || !str2) return false;
+
+  if (typeof str1 !== "string" || typeof str2 !== "string") return false;
+
+  if (str1.length !== str2.length) return false;
+
+  const str1Map = createMap(str1.toLowerCase());
+  const str2Map = createMap(str2.toLowerCase());
+  return compareMaps(str1Map, str2Map)
 }
 
+const createMap = (string) => {
+  const map = new Map();
+  for (let i = 0; i < string.length; i++) {
+    if (!map.has(string.charAt(i))) {
+      map.set(string.charAt(i), 1);
+    } else {
+      const countOfAlphabet = map.get(string.charAt(i)) + 1;
+      map.set(string.charAt(i), countOfAlphabet);
+    }
+  }
+  return map;
+};
+const compareMaps = (map1, map2) => {
+  if (map1.size !== map2.size) return false;
+  const map1Keys = Array.from(map1.keys());
+  let result = true;
+  for (let i = 0; i < map1Keys.length; i++) {
+    if (
+      !map2.has(map1Keys[i]) &&
+      map1.get(map1Keys[i]) !== map2.get(map1Keys[i])
+    ) {
+      result = false;
+      break;
+    }
+  }
+  return result;
+};
+// we hasve to find same count of strings
+isAnagram("Dhruv", "DhuVr");
 module.exports = isAnagram;
