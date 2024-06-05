7151483f3e8c88e08201ce2ee215839c3538e8d1
Dhruv Aggarwal <dhruvaggarwal1503@gmail.com>
2024-06-05 23:40:45 +0530

    sol:error-count

--- week-3/01-middlewares/03-errorcount.js
+++ week-3/01-middlewares/03-errorcount.js
@@ -9,10 +9,22 @@ let errorCount = 0;
 // Your task is to
 // 1. Ensure that if there is ever an exception, the end user sees a status code of 404
 // 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
+app.use((err, req, res, next) => {
+  if (err) {
+    errorCount++;
+    res.sendStatus(404);
+  } else {
+    next();
+  }
+});
 
 app.get('/user', function(req, res) {
-  throw new Error("User not found");
-  res.status(200).json({ name: 'john' });
+  try {
+    throw new Error("User not found");
+    res.status(200).json({ name: 'john' });
+  } catch (err) {
+    next(err);
+  }
 });
 
 app.post('/user', function(req, res) {
@@ -22,5 +34,11 @@ app.post('/user', function(req, res) {
 app.get('/errorCount', function(req, res) {
   res.status(200).json({ errorCount });
 });
+
+app.use((err, req, res, next) => {
+  errorCount++;
+  res.sendStatus(404);
+});
+
 
 module.exports = app;
